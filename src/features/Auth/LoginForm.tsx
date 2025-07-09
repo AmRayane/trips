b56df93e180da7b3e4db0716169df054962ecca3
Amligh Rayane <119620692+AmRayane@users.noreply.github.com>
2025-07-09 08:58:47 +0100

    login page

--- src/features/Auth/LoginForm.tsx
+++ src/features/Auth/LoginForm.tsx
@@ -14,34 +14,41 @@ export default function LoginForm() {
   const disabled: boolean = !email || !password;
 
   return (
-    <div className="flex h-[100vh] items-center justify-center bg-auth bg-cover bg-center">
-      <div className="flex max-w-md flex-col items-center gap-3 rounded-20 bg-white p-6 shadow-500">
+    <div className="relative flex h-[100vh] flex-col items-center justify-center bg-auth bg-cover bg-center">
+      <div className="absolute inset-0 bg-[#ffffff31]"></div>
+      <div className="z-10 mb-3 flex flex-col items-center">
         <Logo />
         <div>
           <h2 className="text-[28px] font-semibold text-dark-100">
-            Admin dashboard login
+            Log in to your account
           </h2>
         </div>
-        <form action="" className="flex w-[100%] flex-col gap-2">
-          <label htmlFor="">Email</label>
-          <input
-            type="text"
-            onChange={(e) => setEmail(e.target.value)}
-            className="h-[30px] rounded-lg bg-[#eaecf0] p-2"
-          />
-          <label htmlFor="">Password</label>
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-            className="h-[30px] rounded-lg bg-[#eaecf0] p-2"
-          />
-          <div className="mt-4 flex w-full gap-2">
+      </div>
+      <div className="z-10 flex w-[420px] max-w-md flex-col items-center gap-3 rounded-20 bg-white p-6 shadow-500">
+        <form action="" className="flex w-[100%] flex-col divide-y-2">
+          <div className="flex items-center justify-between py-3">
+            <label htmlFor="">Email address</label>
+            <input
+              type="text"
+              onChange={(e) => setEmail(e.target.value)}
+              className="h-[30px] rounded-lg border bg-white p-4"
+            />
+          </div>
+          <div className="flex items-center justify-between py-3">
+            <label htmlFor="">Password</label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              className="h-[30px] rounded-lg border bg-white p-4"
+            />
+          </div>
+          <div className="flex w-full gap-2 py-3">
             <button
               disabled={disabled || isLoading}
               onClick={(e) => (e.preventDefault(), login({ email, password }))}
-              className="flex-1 rounded-lg bg-[#eaecf0] px-6 py-3 font-bold text-white disabled:cursor-not-allowed"
+              className="flex-1 rounded-lg bg-dark-100 px-6 py-3 font-bold text-white disabled:cursor-not-allowed"
             >
-              Sign in
+              Log in
             </button>
             <button
               onClick={handleLoginWithGoogle}
